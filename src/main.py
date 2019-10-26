8333f078d8bfd0bdfaae59ba5ce669fa0ac4d4c1
sandeep <sandeep90@hotmail.co.uk>
2019-10-26 18:23:10 +0100

    Initilised start position in matrix

--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ m = 5
 # Initialise matrix
 matrix = np.ones((n, m), dtype=int)
 
-# Initialise start point
-start_x_position = random.choice(value) for value in range(n)
+# Initialise start point away from edge
+start_x_position = random.randrange(1, n-1)
+start_y_position = random.randrange(1, m-1)
+start_position = (start_x_position, start_y_position)
+matrix[start_position] = 2
 
-
-start_point = random.choice(matrix)
-print(start_point)
+def create_escape_path(x_position, y_position):
+    
