cc83277f9442992f3981085cc8b1be534ab916dc
Sandeep <sandeep90@hotmail.co.uk>
2019-10-26 17:22:16 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+
+# Set matrix dimensions
+n = 6
+m = 5
+
+# Initialise matrix
+matrix = np.ones((n, m), dtype=int)
+
+# Initialise start point
+start_x_position = random.choice(value) for value in range(n)
+
+
+start_point = random.choice(matrix)
+print(start_point)
