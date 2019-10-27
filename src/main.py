0ae99638ecc746133898f3fc247e0ecce334931a
sandeep <sandeep90@hotmail.co.uk>
2019-10-27 21:44:21 +0000

    Added user input functionality and fixed coordinate issue.

--- src/main.py
+++ src/main.py
@@ -7,20 +7,20 @@ maze = [[1, 2, 1, 1, 1, 1],
 
 
 def simple_search(x, y, matrix):
-    if matrix[x][y] == 2:
+    if matrix[y][x] == 2:
         print('Exit found at %d,%d' % (x, y))
         return True
-    elif matrix[x][y] == 1:
+    elif matrix[y][x] == 1:
         print('Wall at %d,%d' % (x, y))
         return False
-    elif matrix[x][y] == 3:
+    elif matrix[y][x] == 3:
         print('Visited %d,%d' % (x, y))
         return False
 
     print('Visiting %d,%d' % (x, y))
 
     # Mark visited cell
-    matrix[x][y] = 3
+    matrix[y][x] = 3
 
     # Search in a clockwise fashion starting with position immediately right of starting point
     if ((x < len(matrix) - 1 and simple_search(x + 1, y, matrix))
@@ -31,5 +31,18 @@ def simple_search(x, y, matrix):
 
     return False
 
+print("\n")
+print('A maze is shown below.', \
+      'Open cells are denoted by 0, walls by 1 and exits by 2.')
+print("\n")
 
-simple_search(1, 4, maze)
+for row in maze:
+    print(*row)
+
+print("\n")
+start_x_position = int(input ("Enter starting x-coordinate: "))
+start_y_position = int(input ("Enter starting y-coordinate: "))
+print("\n")
+
+simple_search(start_x_position, start_y_position, maze)
+print('Complete!')
